0d19c2e47e7cd2f67696efffbf756f493fa952ce
Utkucan Doğan <41344400+utkucandogan@users.noreply.github.com>
2022-05-20 08:03:52 +0300

    ToW algorithm implemented

--- 519ProjectTemplate/fhe_template_project.py
+++ 519ProjectTemplate/fhe_template_project.py
@@ -5,6 +5,32 @@ from eva.metric import valuation_mse
 import timeit
 import networkx as nx
 from random import random
+from itertools import combinations
+from math import inf
+
+# TOW Solution Implementation
+graphSize = 0
+subset = []
+
+class TOW:
+    def __init__(self):
+        self._min = inf
+        self._ind = None
+
+    def compare(self, diff, i):
+        diff = abs(diff)
+        if diff < self._min:
+            self._min = diff
+            self._ind = i
+
+    def solution(self):
+        # Reproduce the solution
+        global graphSize
+        gs = graphSize
+        for i, sol in enumerate(combinations(range(gs), gs//2)):
+            if i == self._ind:
+                return sol
+        return None # Error
 
 # Using networkx, generate a random graph
 # You can change the way you generate the graph
@@ -56,12 +82,19 @@ def prepareInput(n, m):
 # You will implement this service based on your selected algorithm
 # you can other parameters using global variables !!! do not change the signature of this function 
 def graphanalticprogram(graph):
-    reval = graph<<1 ## Check what kind of operators are there in EVA, this is left shift
-    # Note that you cannot compute everything using EVA/CKKS
-    # For instance, comparison is not possible
-    # You can add, subtract, multiply, negate, shift right/left
-    # You will have to implement an interface with the trusted entity for comparison (send back the encrypted values, push the trusted entity to compare and get the comparison output)
-    return reval
+    global graphSize
+
+    gs = graphSize
+    sum = 0
+    for i in range(gs):
+        sum += graph << i
+    
+    ssum = 0
+    for i in subset:
+        ssum += graph << i
+    diff = ssum * 2 - sum
+
+    return diff
     
 # Do not change this 
 # the parameter n can be passed in the call from simulate function
@@ -90,46 +123,81 @@ def simulate(n):
     config['balance_reductions'] = 'true'
     inputs = prepareInput(n, m)
 
-    graphanaltic = EvaProgramDriver("graphanaltic", vec_size=m,n=n)
-    with graphanaltic:
-        graph = Input('Graph')
-        reval = graphanalticprogram(graph)
-        Output('ReturnedValue', reval)
+    global graphSize
+    gs = n*n
+    graphSize = gs
+
+    tow = TOW()
+
+    compiletime = 0
+    keygenerationtime = 0
+    encryptiontime = 0
+    executiontime = 0
+    decryptiontime = 0
+    referenceexecutiontime = 0
+    mse = 0
+
+    global subset
+    for i, s in enumerate(combinations(range(gs), gs//2)):
+        print(f"Loop {i}")
+
+        subset = s
+
+        graphanaltic = EvaProgramDriver("graphanaltic", vec_size=m,n=n)
+        with graphanaltic:
+            graph = Input('Graph')
+            reval = graphanalticprogram(graph)
+            Output('ReturnedValue', reval)
+        print(f"Program Driver Done")
+
+        prog = graphanaltic
+        prog.set_output_ranges(30)
+        prog.set_input_scales(30)
+
+        start = timeit.default_timer()
+        compiler = CKKSCompiler(config=config)
+        compiled_multfunc, params, signature = compiler.compile(prog)
+        compiletime += (timeit.default_timer() - start) * 1000.0 #ms
+        print(f"Compiler Done")
+
+        start = timeit.default_timer()
+        public_ctx, secret_ctx = generate_keys(params)
+        keygenerationtime += (timeit.default_timer() - start) * 1000.0 #ms
+        print(f"Key Generation Done")
+        
+        start = timeit.default_timer()
+        encInputs = public_ctx.encrypt(inputs, signature)
+        encryptiontime += (timeit.default_timer() - start) * 1000.0 #ms
+        print(f"Encryption Done")
+
+        start = timeit.default_timer()
+        encOutputs = public_ctx.execute(compiled_multfunc, encInputs)
+        executiontime += (timeit.default_timer() - start) * 1000.0 #ms
+        print(f"Execution Done")
+
+        start = timeit.default_timer()
+        outputs = secret_ctx.decrypt(encOutputs, signature)
+        decryptiontime += (timeit.default_timer() - start) * 1000.0 #ms
+        print(f"Decryption Done")
+
+        start = timeit.default_timer()
+        reference = evaluate(compiled_multfunc, inputs)
+        referenceexecutiontime += (timeit.default_timer() - start) * 1000.0 #ms
+        print(f"Evaluation Done")
+
+        mse += valuation_mse(outputs, reference) # since CKKS does approximate computations, this is an important measure that depicts the amount of error
+
+        tow.compare(outputs['ReturnedValue'][0], i)
+
+    solution = tow.solution()
+    print("Solution")
+    print(solution)
     
-    prog = graphanaltic
-    prog.set_output_ranges(30)
-    prog.set_input_scales(30)
+    # # Change this if you want to output something or comment out the two lines below
+    # for key in outputs:
+    #     print(key, float(outputs[key][0]), float(reference[key][0]))
 
-    start = timeit.default_timer()
-    compiler = CKKSCompiler(config=config)
-    compiled_multfunc, params, signature = compiler.compile(prog)
-    compiletime = (timeit.default_timer() - start) * 1000.0 #ms
-
-    start = timeit.default_timer()
-    public_ctx, secret_ctx = generate_keys(params)
-    keygenerationtime = (timeit.default_timer() - start) * 1000.0 #ms
-    
-    start = timeit.default_timer()
-    encInputs = public_ctx.encrypt(inputs, signature)
-    encryptiontime = (timeit.default_timer() - start) * 1000.0 #ms
-
-    start = timeit.default_timer()
-    encOutputs = public_ctx.execute(compiled_multfunc, encInputs)
-    executiontime = (timeit.default_timer() - start) * 1000.0 #ms
-
-    start = timeit.default_timer()
-    outputs = secret_ctx.decrypt(encOutputs, signature)
-    decryptiontime = (timeit.default_timer() - start) * 1000.0 #ms
-
-    start = timeit.default_timer()
-    reference = evaluate(compiled_multfunc, inputs)
-    referenceexecutiontime = (timeit.default_timer() - start) * 1000.0 #ms
-    
-    # Change this if you want to output something or comment out the two lines below
-    for key in outputs:
-        print(key, float(outputs[key][0]), float(reference[key][0]))
-
-    mse = valuation_mse(outputs, reference) # since CKKS does approximate computations, this is an important measure that depicts the amount of error
+    mse = mse / i
 
     return compiletime, keygenerationtime, encryptiontime, executiontime, decryptiontime, referenceexecutiontime, mse
 
@@ -143,7 +211,7 @@ if __name__ == "__main__":
     resultfile.close()
     
     print("Simulation campaing started:")
-    for nc in range(36,64,4): # Node counts for experimenting various graph sizes
+    for nc in range(4,64,4): # Node counts for experimenting various graph sizes
         n = nc
         resultfile = open("results.csv", "a") 
         for i in range(simcnt):
